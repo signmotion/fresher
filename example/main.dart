c363f7f91639652de30219ed650ccf8469d2ac97
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-17 18:16:48 +0200

    Commented an example.

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: avoid_print
 
 void main() {
-  // TODO
+  // run in command line from the project folder:
+  // dart bin/fresher.dart path/to/you_project_base
+
+  // see `test/bin/main.dart` for examples of usage
 }
