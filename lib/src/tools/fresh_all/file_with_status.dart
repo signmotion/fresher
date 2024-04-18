457111d02f248b0f0276b2d95147f8ffdaca0a13
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-18 18:50:12 +0200

    Provided a priority.

--- lib/src/tools/fresh_all/file_with_status.dart
+++ lib/src/tools/fresh_all/file_with_status.dart
@@ -1,6 +1,6 @@
 part of 'bloc.dart';
 
-class FileWithStatus {
+class FileWithStatus implements Comparable<FileWithStatus> {
   const FileWithStatus({
     required this.file,
     required this.status,
@@ -9,18 +9,25 @@ class FileWithStatus {
   final FreshFile file;
   final UpdatedStatus status;
 
+  /// ! Group by [status.priority].
+  @override
+  int compareTo(FileWithStatus other) => '${status.priority} $file'
+      .compareTo('${other.status.priority} ${other.file}');
+
   @override
   String toString() => '${status.name}\t$file';
 }
 
 enum UpdatedStatus {
-  undspecified(),
-  added(),
-  modified(),
-  skipped(),
-  unchanged();
+  undspecified(100),
+  added(2),
+  modified(1),
+  skipped(3),
+  unchanged(4);
 
-  const UpdatedStatus();
+  const UpdatedStatus(this.priority);
+
+  final int priority;
 
   String get coloredName => switch (name) {
         'unspecified' => spacedName.white.onRed,
