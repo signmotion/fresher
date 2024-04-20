4aaee478fc3c145cd9d719a4d1b6a64cc18c715b
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-20 18:08:16 +0200

    Redirected FresherEnumExt from List to Iterable.

--- lib/src/extensions/enum.dart
+++ lib/src/extensions/enum.dart
@@ -1,7 +1,7 @@
 part of '../../fresher.dart';
 
 /// ! Copied from <https://github.com/signmotion/dart_helper>.
-extension FresherEnumExt on List<Enum> {
+extension FresherEnumExt on Iterable<Enum> {
   /// Example:
   /// ```
   /// UnitType.values.findByName(s, defaults: UnitType.m);
