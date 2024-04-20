c5dc96de41f8b895515f9e3424c5a473fd5c6bbf
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-20 22:01:26 +0200

    Enhanced Pubspec.

--- lib/src/pubspec.dart
+++ lib/src/pubspec.dart
@@ -3,34 +3,50 @@ part of '../fresher.dart';
 /// A `pubspec.yaml` file.
 class Pubspec extends Equatable implements Comparable<Pubspec> {
   Pubspec(this.pathToProject) {
-    if (!exists) {
-      throw PathNotFoundException(pathToFile, const OSError());
+    if (!existsYaml) {
+      throw PathNotFoundException(pathToFileYaml, const OSError());
     }
   }
 
-  static const filename = 'pubspec.yaml';
+  static const filenameYaml = 'pubspec.yaml';
+  static const filenameLock = 'pubspec.lock';
 
-  /// A path to project folder that contains [filename].
+  /// A path to project folder that contains [filenameYaml].
   final String pathToProject;
 
   /// A path to `pubspec.yaml` file for [projectId].
-  String get pathToFile => p.join(pathToProject, filename).npath;
+  String get pathToFileYaml => p.join(pathToProject, filenameYaml).npath;
 
-  WFile get file => WFile(pathToFile);
+  /// A path to `pubspec.lock` file for [projectId].
+  String get pathToFileLock => p.join(pathToProject, filenameLock).npath;
 
-  bool get exists => file.existsFile();
+  WFile get fileYaml => WFile(pathToFileYaml);
 
-  String get rawContent => file.readAsText()!;
+  WFile get fileLock => WFile(pathToFileLock);
 
-  YamlMap get content => loadYaml(rawContent) as YamlMap;
+  bool get existsYaml => fileYaml.existsFile();
+
+  bool get existsLock => fileLock.existsFile();
+
+  String get rawContentYaml => fileYaml.readAsText()!;
+
+  String get rawContentLock => fileLock.readAsText()!;
+
+  YamlMap get contentYaml => loadYaml(rawContentYaml) as YamlMap;
+
+  YamlMap get contentLock => loadYaml(rawContentLock) as YamlMap;
+
+  /// Deletes a [pathToFileLock].
+  void removeLock() => fileLock.delete();
 
   @override
   List<Object?> get props => [pathToProject];
 
-  /// ! Compare by [pathToFile].
+  /// ! Compare by [pathToFileYaml].
   @override
-  int compareTo(Pubspec other) => pathToFile.compareTo(other.pathToFile);
+  int compareTo(Pubspec other) =>
+      pathToFileYaml.compareTo(other.pathToFileYaml);
 
   @override
-  String toString() => pathToFile;
+  String toString() => pathToFileYaml;
 }
