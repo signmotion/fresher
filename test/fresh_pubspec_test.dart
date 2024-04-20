5ece6e7f70b361ef556f91650f16c76aaa07789a
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-20 18:11:53 +0200

    More tests.

--- test/fresh_pubspec_test.dart
+++ test/fresh_pubspec_test.dart
@@ -1,12 +1,33 @@
 import 'package:fresher/fresher.dart';
+import 'package:json_dart/json_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('correct data', () {
+  group('correct data, outdatedAsJson', () {
+    test('pubspec.yaml, no pubspec.lock', () async {
+      final pubspec = FreshPubspec('test/data/pubspec_yaml_only');
+      final r = await pubspec.outdatedAsJson;
+      expect(r.length, 4, reason: r.sjson);
+    });
+
     test('pubspec.lock + pubspec.yaml', () async {
       final pubspec = FreshPubspec('test/data/pubspec_lock_and_yaml');
       final r = await pubspec.outdatedAsJson;
-      expect(r.length, 0, reason: '$r');
+      expect(r.length, 0, reason: r.sjson);
     });
-  }, tags: ['current']);
+  });
+
+  group('correct data, outdated', () {
+    test('pubspec.yaml, no pubspec.lock', () async {
+      final pubspec = FreshPubspec('test/data/pubspec_yaml_only');
+      final r = await pubspec.outdated;
+      expect(r.length, 4, reason: r.sjson);
+    });
+
+    test('pubspec.lock + pubspec.yaml', () async {
+      final pubspec = FreshPubspec('test/data/pubspec_lock_and_yaml');
+      final r = await pubspec.outdated;
+      expect(r.length, 0, reason: r.sjson);
+    });
+  });
 }
