3404abb74c731e2bf9bd19036cd427d16f7ef6f8
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-21 15:58:11 +0200

    Added a test for GettingSdksEvent.

--- test/fresh_all_test.dart
+++ test/fresh_all_test.dart
@@ -1,7 +1,25 @@
+import 'dart:io';
+
+import 'package:fresher/fresher.dart';
+import 'package:fresher/src/tools/fresh_all/bloc.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
+/// ! See CLI tests in `test/bin/fresher_test.dart`.
 void main() {
-  test('FreshAll', () {
-    // see `test/bin/fresher.dart`
-  });
+  final source = p.join('test', 'data', 'all_projects');
+  final options = FresherOptions()
+    ..sourceDirectory = Directory(source)
+    ..leaveSpaces = false;
+
+  group('GettingSdksEvent', () {
+    final bloc = FreshAllBloc(options);
+
+    test('check', () async {
+      bloc.add(const GettingSdksEvent());
+      await bloc.allCompleted();
+
+      expect(bloc.state.sdks, const ['dart', 'flutter']);
+    });
+  }, tags: ['current']);
 }
