3864245a91306fd6f69732162c6588e1de0921f3
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-21 15:57:47 +0200

    Provided a stat with all reviewed packages with statuses.

--- lib/src/tools/fresh_all/result.dart
+++ lib/src/tools/fresh_all/result.dart
@@ -9,25 +9,56 @@ class FreshAllResultRunner extends ResultRunner {
 
   final FreshAllState state;
 
-  @override
-  String toString() {
+  /// All rewieved files with statuses.
+  String statFiles() {
     var r = '';
 
+    r += 'Files: ${state.filesWithStatus.length}$newLine';
     for (final e in state.filesWithStatus.entries) {
       final projectKey = e.key;
-      r += '\n$projectKey\n';
+      r += '$newLine${projectKey.blueBright}$newLine';
       final table = Table(
         header: ['Status'.whiteBright, 'File'.whiteBright],
         columnAlignment: [HorizontalAlign.right, HorizontalAlign.left],
       );
       // group and sort
       final sorted = List.of(e.value, growable: false)..sort();
-      for (final fs in sorted) {
-        table.add([fs.status.coloredName, fs.file.key]);
+      for (final l in sorted) {
+        final s = l.file.key;
+        table.add([l.status.coloredName, s]);
       }
       r += '$table';
     }
 
     return r;
   }
+
+  /// All rewieved packages with statuses.
+  String statPackages() {
+    var r = '';
+
+    r += 'Packages: ${state.packagesWithStatus.length}$newLine';
+    for (final e in state.packagesWithStatus.entries) {
+      final projectKey = e.key;
+      r += '$newLine${projectKey.blueBright}$newLine';
+      final table = Table(
+        header: ['Status'.whiteBright, 'Package'.whiteBright],
+        columnAlignment: [HorizontalAlign.right, HorizontalAlign.left],
+      );
+      // group and sort
+      final sorted = List.of(e.value, growable: false)..sort();
+      for (final l in sorted) {
+        final s = '${l.package.id} ${l.package.currentYaml} '
+                '${l.status == UpdatedPackageStatus.modified ? l.package.resolvable : ""}'
+            .trim();
+        table.add([l.status.coloredName, s]);
+      }
+      r += '$table';
+    }
+
+    return r;
+  }
+
+  @override
+  String toString() => '${statFiles()}$newLine${statPackages()}';
 }
